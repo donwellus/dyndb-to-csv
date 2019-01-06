b3f91e4fbd48d02ece8d1267fabff7bbffa4453b
Daniel Tre <daniel.tre@gmail.com>
2019-01-05 23:56:31 -0200

    Created functions to extract header and rows

--- dyndb2csv.py
+++ dyndb2csv.py
@@ -17,4 +17,26 @@ def cli(input):
         dyndb2csv foo.txt
     """
     data = json.load(input)
-    print(data)
+
+    header_keys = get_keys(data['Items'])
+    print(header_keys)
+
+    for item in data['Items']:
+        i = get_row(item)
+        print(i)
+
+def get_keys(items):
+    head = {}
+    for item in items:
+        for col in item:
+            head[col] = True
+    return head.keys()
+
+def get_row(item):
+    row = {}
+    for col, val in item.items():
+        key = val.keys()[0]
+        if key in ['S','N','BOOL','B']:
+            row[col] = val[key]
+
+    return row
